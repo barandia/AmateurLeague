d4c9bd7b5a85929b991a83852ba513be37b81ace
Benjie Arandia <arandia.benjie@gmail.com>
2019-06-17 14:29:11 -0700

    Added TeamManager factory class for managing teams

--- src/AmatuerLeague/Team.cs
+++ src/AmatuerLeague/Team.cs
@@ -12,7 +12,7 @@ namespace AmatuerLeague
         public string Id { get; private set;}
         public string Name { get; set;}
         // Todo: const or readonly? Need to figure out how to set this property only once.
-        public string LeagueId { get; private set;}   
+        public string LeagueId { get; set;}   
         public Player Captain { get; set;}
         public Player CoCaptain { get; set;} 
         public List<Player> Roster = new List<Player>();
